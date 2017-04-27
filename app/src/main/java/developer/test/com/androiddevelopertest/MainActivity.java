3b8b69682b864a5a5d15cea2a96f145a80efe419
fameempathy <fame_empathy@o2.pl>
2017-04-27 16:29:36 +0200

    Adding the display of the repos, search feature and infinite loading

--- app/src/main/java/developer/test/com/androiddevelopertest/MainActivity.java
+++ app/src/main/java/developer/test/com/androiddevelopertest/MainActivity.java
@@ -5,8 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import developer.test.com.androiddevelopertest.ResponseClasses.Item;
+import developer.test.com.androiddevelopertest.ResponseClasses.Repos;
+import developer.test.com.androiddevelopertest.Utils.ApiClient;
+import developer.test.com.androiddevelopertest.Utils.ReposArrayAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements AbsListView.OnScrollListener, View.OnClickListener {
+    private ListView reposList;
+    private ArrayList<Item> allItems;
+    private int page = 0;
+    private final int PER_PAGE = 10;
+    private int currentLast;
+    private ApiClient restClient;
+    private EditText inputEditText;
+    private Button button;
+    private String currentQuery = "tetris";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +42,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        //could have used butterknife or databinding library but I'm not sure whether I should
+        reposList = (ListView) findViewById(R.id.listView);
+        reposList.setOnScrollListener(this);
+
+        button = (Button) findViewById(R.id.request);
+        button.setOnClickListener(this);
+
+        inputEditText = (EditText) findViewById(R.id.userInput);
+
+        allItems = new ArrayList<>();
+
+        restClient = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.github.com/")
+                .build()
+                .create(ApiClient.class);
+
+        loadItems();
+
+
+    }
+
+    private void loadItems() {
+        Call<Repos> reposCall = restClient.searchForRepos(currentQuery, PER_PAGE, page);
+
+        reposCall.enqueue(new Callback<Repos>() {
+            @Override
+            public void onResponse(Call<Repos> call, Response<Repos> response) {
+                Repos responseRepos = response.body();
+                if (responseRepos != null) {
+                    allItems.addAll(responseRepos.getItems());
+                    ReposArrayAdapter reposArrayAdapter = new ReposArrayAdapter(MainActivity.this, allItems);
+                    reposList.setAdapter(reposArrayAdapter);
+                } else {
+                    //limit was reached
+                    currentLast = 0;
+                    Toast.makeText(MainActivity.this, "You've reached limit of 10 API calls per minute. Please wait a bit.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Repos> call, Throwable t) {
+                call.cancel();
+            }
+        });
     }
 
     @Override
@@ -37,5 +110,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int i) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
+        switch (absListView.getId()) {
+            case R.id.listView:
+                final int lastItem = firstVisibleItem + visibleItemCount;
+                //check whether user has scrolled to the end
+                if (lastItem == totalItemCount) {
+                    if (currentLast != lastItem) {
+                        //to avoid multiple calls for last item
+                        currentLast = lastItem;
+
+                        page++;
+                        loadItems();
+                    }
+                }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.request) {
+            String input = inputEditText.getText().toString();
+            if(!input.isEmpty()) {
+                currentQuery = input;
+
+                //reset values
+                currentLast = 0;
+                page = 0;
+                allItems.clear();
+
+                loadItems();
+            }
+        }
+
     }
 }
--- app/src/main/java/developer/test/com/androiddevelopertest/Utils/ReposArrayAdapter.java
+++ app/src/main/java/developer/test/com/androiddevelopertest/Utils/ReposArrayAdapter.java
@@ -1,11 +1,14 @@
 package developer.test.com.androiddevelopertest.Utils;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,12 +19,14 @@ import developer.test.com.androiddevelopertest.ResponseClasses.Item;
 public class ReposArrayAdapter extends ArrayAdapter<Item> {
     private Context context;
     private ArrayList<Item> items;
+    private Resources resources;
 
 
     public ReposArrayAdapter(Context context, ArrayList<Item> objects) {
         super(context, R.layout.single_item_layout, objects);
         this.context = context;
         items = objects;
+        resources = context.getResources();
     }
 
     @NonNull
@@ -38,6 +43,7 @@ public class ReposArrayAdapter extends ArrayAdapter<Item> {
 
             convertView.setTag(viewHolder);
 
+            viewHolder.singleLayout = (LinearLayout) convertView.findViewById(R.id.single_item);
             viewHolder.name = (TextView) convertView.findViewById(R.id.name);
             viewHolder.owner = (TextView) convertView.findViewById(R.id.owner);
             viewHolder.size = (TextView) convertView.findViewById(R.id.size);
@@ -45,15 +51,20 @@ public class ReposArrayAdapter extends ArrayAdapter<Item> {
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
+        Item item = items.get(position);
+        viewHolder.name.setText(resources.getString(R.string.name, item.getName()));
+        viewHolder.owner.setText(resources.getString(R.string.login, item.getOwner().getLogin()));
+        viewHolder.size.setText(resources.getString(R.string.size, item.getSize()));
 
-        viewHolder.name.setText(items.get(position).getName());
-        viewHolder.owner.setText(items.get(position).getOwner().getLogin());
-        viewHolder.size.setText(String.valueOf(items.get(position).getSize()));
+        if (item.getHasWiki()) {
+            viewHolder.singleLayout.setBackgroundColor(Color.LTGRAY);
+        }
 
         return convertView;
     }
 
     private static class ViewHolder {
+        private LinearLayout singleLayout;
         private TextView name;
         private TextView owner;
         private TextView size;
